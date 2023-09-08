2ea8be9d27a21191c015c1a1bbd510566db832f4
Jorge González <steinkel@gmail.com>
2023-09-08 11:13:50 +0100

    initial bake all

---
+++ tests/Fixture/FilesFixture.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * FilesFixture
+ */
+class FilesFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'group_id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'type' => 'Lorem ipsum dolor sit amet',
+                'path' => 'Lorem ipsum dolor sit amet',
+                'metadata' => '',
+                'created' => 1692784325,
+                'modified' => 1692784325,
+            ],
+        ];
+        parent::init();
+    }
+}
---
+++ tests/Fixture/GroupsFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * GroupsFixture
+ */
+class GroupsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'created' => 1692784325,
+                'modified' => 1692784325,
+            ],
+        ];
+        parent::init();
+    }
+}
